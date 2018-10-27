aef57b2802bcabacbea2ac5e27b785ce9533b157
Patrick Mendoza <patrickmendoza@Patricks-MacBook-Air.local>
2018-10-27 07:19:16 -0700

    pseudocode

---
+++ assets/js/gify.js
@@ -0,0 +1,61 @@
+// define variables
+// populate a beginning array of search words 
+
+
+// there will need to be a function to populate the buttons to the html
+// there will need to be a funtion to use the word from each button to get the gifs from gify and display them when clicked, and that should start by clearing the gifs present and doing the ajax get function again
+// start with the animal example
+// function to initialize the buttons on page loadup or refresh
+
+// when I add the additional buttons Form, the form should add a button, not perform the gify query.  all it should do is append the array and run the function to populate the buttons again
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$("button").on("click", function() {
+    var animal = $(this).attr("data-animal");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+      animal + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+
+
+      console.log(response);
+
+
+      var results = response.data
+
+
+      for (var i = 0; i < results.length; i++) {
+
+
+      var animalDiv = $("<div>");
+
+      var p = $("<p>").html(results[i].rating);
+     
+      var animalImage = $("<img>");
+        animalImage.attr("src", results[i].images.fixed_height.url)
+      animalDiv.append(p)
+      animalDiv.append(animalImage)
+      $("#gif-appear-here").prepend(animalDiv);
+
+
+      }
+
+    });
+  });
